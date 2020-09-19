119bb99c4e88790231b4819e530634ecaf5fad31
Yohan Cauvry <ycauvry@gmail.com>
2020-09-19 21:09:52 +0200

    Revert "first commit before setting server"
    This reverts commit 94e609f926b5f6c736625e0e1cdba0bf1c6dfb0f.

--- public/js/dom-mangement.js
+++
@@ -1,8 +0,0 @@
-const triggerMenu = document.querySelectorAll('.js-trigger-menu');
-const mainMenu = document.getElementById('main-menu');
-
-triggerMenu.forEach(trigger => {
-    trigger.addEventListener('click', () => {
-        mainMenu.classList.toggle('visible');
-    })
-})
